0af2a9df1f538f9cfbef37b2c0aed4472195addd
yourbeagle <mainwattson3135@gmail.com>
2023-07-30 20:47:34 +0700

    change minor mistake

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = "mysql+pymysql://root:agHbxvG3PpWgGs9PEpfB@containers-us-west-35.railway.app:7763/railway"
+URL_DATABASE = "mysql+pymysql://root@localhost:3306/maxxitani"
 
 engine = create_engine(URL_DATABASE)
 
